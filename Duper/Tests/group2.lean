ce63b30cdebc42c2c7d7d1ced6750a6772dcf6e1
Alexander Bentkamp <bentkamp@gmail.com>
2022-11-18 10:51:14 +0100

    workaround for group theory example

--- Duper/Tests/group2.lean
+++ Duper/Tests/group2.lean
@@ -20,24 +20,24 @@ theorem test : x ⬝ one = x :=
 by duper [@Group.mul_one]
 
 theorem exists_right_inv : inv x ⬝ x = e :=
-by duper [@Group.mul_assoc, @Group.mul_one, @Group.mul_inv]
+by duper [@Group.mul_assoc G hG, @Group.mul_one G hG, @Group.mul_inv G hG]
 
 theorem left_neutral_unique (x : G) : (∀ y, x ⬝ y = y) → x = e :=
-by duper [@Group.mul_assoc, @Group.mul_one, @Group.mul_inv]
+by duper [@Group.mul_assoc G hG, @Group.mul_one G hG, @Group.mul_inv G hG]
 
 theorem right_neutral_unique (x : G) : (∀ y, y ⬝ x = y) → x = e :=
-by duper [@Group.mul_assoc, @Group.mul_one, @Group.mul_inv]
+by duper [@Group.mul_assoc G hG, @Group.mul_one G hG, @Group.mul_inv G hG]
 
 theorem right_inv_unique (x y z : G) (h : x ⬝ y = e) (h : x ⬝ z = e) : y = z :=
-by duper [@Group.mul_assoc, @Group.mul_one, @Group.mul_inv]
+by duper [@Group.mul_assoc G hG, @Group.mul_one G hG, @Group.mul_inv G hG]
 
 theorem left_inv_unique (x y z : G) (h : y ⬝ x = e) (h : z ⬝ x = e) : y = z :=
-by duper [@Group.mul_assoc, @Group.mul_one, @Group.mul_inv]
+by duper [@Group.mul_assoc G hG, @Group.mul_one G hG, @Group.mul_inv G hG]
 
 noncomputable def sq := x ⬝ x
 
 theorem sq_mul_sq_eq_e (h_comm : ∀ (a b : G), a ⬝ b = b ⬝ a) (h : x ⬝ y = e) :
   sq x ⬝ sq y = e :=
-by duper [@sq, @Group.mul_assoc, @Group.mul_one, @Group.mul_inv]
+by duper [@sq, @Group.mul_assoc G hG, @Group.mul_one G hG, @Group.mul_inv G hG]
 
 end Group
