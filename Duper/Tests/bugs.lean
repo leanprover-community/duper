63dd87b2fa10191306994d99a8cbddb5188aa0ea
IndPrinciple <syxx2013050321@163.com>
2023-02-23 19:20:13 +0800

    Misc change

--- Duper/Tests/bugs.lean
+++ Duper/Tests/bugs.lean
@@ -6,7 +6,7 @@ import Duper.Tactic
 -- tptp COM035_5 "../TPTP-v8.0.0/Problems/COM/COM025_5.p"
 --   by duper
 
-set_option maxHeartbeats 100000 in
+set_option maxHeartbeats 200000 in
 tptp SYN007 "../TPTP-v8.0.0/Problems/SYN/SYN007+1.014.p"
   by duper
 
