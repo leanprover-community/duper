be0f1ce4ff74a36aaa4c9b77218debbd748a7911
Alexander Bentkamp <bentkamp@gmail.com>
2023-03-09 10:11:51 +0100

    recursor tests

--- Duper/Tests/test_recursor.lean
+++ Duper/Tests/test_recursor.lean
@@ -7,3 +7,28 @@ def rec₁ : False := by
   duper [Nat.rec]
 
 #check Nat.rec
+
+namespace Color1
+
+inductive Color :=
+| red : Color
+
+example : @Color.rec (fun _ => Nat) a .red = a := by duper [Color.rec]
+
+end Color1
+
+namespace Color2
+
+inductive Color :=
+| red : Color
+| green : Color
+
+set_option trace.DUnif.result true
+set_option dUnifDbgOn true
+set_option trace.Superposition.debug true
+set_option trace.Saturate.debug true
+
+example : @Color.rec (fun _ => Nat) a b .red = a := by duper [Color.rec]
+
+
+end Color2
