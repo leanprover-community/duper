fb7a1d4f8ae9318e131d2e22969da379ad79e1c1
Alexander Bentkamp <bentkamp@gmail.com>
2023-03-09 17:21:00 +0100

    more recursor tests

--- Duper/Tests/test_recursor.lean
+++ Duper/Tests/test_recursor.lean
@@ -31,4 +31,26 @@ set_option trace.Saturate.debug true
 example : @Color.rec (fun _ => Nat) a b .red = a := by duper [Color.rec]
 
 
+def test : Color → Color
+| .red => .green
+| .green => .red
+
+set_option pp.match false
+#print test
+#print test.match_1
+#print Color.casesOn
+
+-- Not sure why this does not work:
+example : test .red = .green := by duper [test, test.match_1, Color.rec, Color.casesOn]
+
 end Color2
+
+
+set_option simultaneousSuperposition false -- TODO: There is a bug in simultaneous sup that prevents this example from working
+set_option trace.DUnif.result true
+set_option dUnifDbgOn true
+set_option trace.Superposition.debug true
+set_option trace.Saturate.debug true
+-- set_option maxHeartbeats 200
+example : @Nat.rec (fun _ => Bool) a b Nat.zero = a := by duper [Nat.rec]
+
