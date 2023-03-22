fcf27bade5d6074c5c29f8cc082956590db8ec57
IndPrinciple <syxx2013050321@163.com>
2023-03-22 10:48:43 +0800

    Commit again for github action

--- Duper/Tests/bugs.lean
+++ Duper/Tests/bugs.lean
@@ -1,6 +1,13 @@
 import Duper.TPTP
 import Duper.Tactic
 
+-- Deterministic timeout
+-- More saturate loops than commit `c1afee181ad15b58edba6972d3acef4e85eba7ed`
+--   "support for recursors"
+set_option maxHeartbeats 200000
+set_option trace.Meta.debug true in
+tptp GEO192 "../TPTP-v8.0.0/Problems/GEO/GEO192+2.p" by duper
+
 -- βη reduction bug
 set_option trace.Meta.debug true in
 set_option trace.Prover.saturate true in
