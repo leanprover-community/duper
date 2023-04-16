0a9f807740ce7f928345a9219662a6b4ea96f8ed
JOSHCLUNE <josh.seth.clune@gmail.com>
2023-04-15 20:42:20 -0400

    Minor change to betaEtaReduce

--- Duper/Util/BetaEtaReduce.lean
+++ Duper/Util/BetaEtaReduce.lean
@@ -6,29 +6,19 @@ open Lean
 open Meta
 open Core
 
-/-- Applies beta and eta reduction exhaustively -/
-partial def betaEtaReduce (e : Expr) : MetaM Expr := do
-  let e' ← Core.betaReduce e
-  match e'.etaExpanded? with
-  | some e' =>
-    if e' == e then return e
-    else betaEtaReduce e'
-  | none =>
-    if e' == e then return e
-    else betaEtaReduce e'
+/-- Applies beta and eta reduction exhaustively. -/
+def betaEtaReduce (e : Expr) : MetaM Expr := do
+  let e ← Core.betaReduce e
+  match e.etaExpanded? with
+  | some e => return e
+  | none => return e
 
-/-- This is the same as Core.betaReduce except that we set useZeta to true since we intend to zetaReduce as well -/
-def betaReduce (e : Expr) : CoreM Expr :=
-  transform e (pre := fun e => return if e.isHeadBetaTarget (useZeta := true) then .visit e.headBeta else .continue)
-
-/-- Applies beta, eta, and zeta reduction exhaustively -/
-partial def betaEtaZetaReduce (e : Expr) : MetaM Expr := do
-  let e' ← betaReduce e
-  let e' ← Meta.zetaReduce e'
-  match e'.etaExpanded? with
-  | some e' =>
-    if e' == e then return e
-    else betaEtaZetaReduce e'
-  | none =>
-    if e' == e then return e
-    else betaEtaZetaReduce e'
+/-- Applies beta, eta, and zeta reduction exhaustively. I believe that by applying zeta reduction
+    first, then beta reduction, and finally eta reduction, it is not necessary to iterate this
+    function until a fixed point is reached (the output should always be a fixed point) -/
+def betaEtaZetaReduce (e : Expr) : MetaM Expr := do
+  let e ← Meta.zetaReduce e
+  let e ← betaReduce e
+  match e.etaExpanded? with
+  | some e => return e
+  | none => return e
