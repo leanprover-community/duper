450e77af37a38655d80559da97ffe95a1de63d16
Alexander Bentkamp <bentkamp@gmail.com>
2023-05-15 12:02:34 +0200

    more bugs

---
+++ Duper/Tests/morebugs.lean
@@ -0,0 +1,42 @@
+import Duper.Tactic
+
+
+
+
+namespace Axioms
+
+axiom X : Type
+axiom f : (X → Prop) → Prop
+
+
+theorem about (s : X → Prop) : f s :=
+sorry
+
+-- This should be really easy
+example (s : X → Prop) : f s :=
+by duper [about]
+
+end Axioms
+
+
+
+
+namespace Seq
+
+instance : Neg Nat := sorry
+
+def even_fun (f : Nat → Nat) := ∀ x, f (-x) = f x
+
+instance : Add (Nat → Nat) := (⟨fun f g x => f x + g x⟩ :  Add (Nat → Nat))
+
+theorem add_apply (f g : Nat → Nat) (x : Nat) : (f + g) x = f x + g x := sorry
+
+example (f g : Nat → Nat) (hf : even_fun f) (hg : even_fun g) : even_fun (f + g) := by
+  unfold even_fun at *
+  duper [add_apply f g]
+
+-- This should be fairly easy, too, by unfolding even_fun as above.
+example (f g : Nat → Nat) (hf : even_fun f) (hg : even_fun g) : even_fun (f + g) := by
+  duper [even_fun, add_apply f g]
+
+end Seq
