097eba3f94aec176d06d29ebb7cc250992bfab26
IndPrinciple <syxx2013050321@163.com>
2023-05-16 18:18:47 +0800

    Adding but not enabling eta-long normal form

--- Duper/Util/BetaEtaReduce.lean
+++ Duper/Util/BetaEtaReduce.lean
@@ -6,6 +6,47 @@ open Lean
 open Meta
 open Core
 
+/-- Eta-expand a beta-reduced expression -/
+partial def etaLong (e : Expr) : MetaM Expr := do
+  match e with
+  | Expr.forallE .. =>
+    forallTelescope e fun xs b => do
+      let bl ← etaLong b
+      mkForallFVarsEtaLong xs bl
+  | Expr.lam .. =>
+    lambdaTelescope e fun xs b => do
+      let bl ← etaLong b
+      mkLambdaFVarsEtaLong xs bl
+  | Expr.letE .. => do
+      throwError "There should be no let binders because they should have been reduced during preprocessing"
+  | Expr.app .. => do
+      let e' ← etaExpand e
+      if e'.isApp then
+        e'.withApp fun f args => do
+          return mkAppN f (← args.mapM etaLong)
+      else
+        etaLong e
+  | Expr.proj _ _ b => return e.updateProj! (← etaLong b)
+  | Expr.mdata _ b => etaLong b
+  | _ => etaExpand e
+where
+  mkForallFVarsEtaLong (xs : Array Expr) (b : Expr) : MetaM Expr := do
+    let mut ret := b
+    for x in xs.reverse do
+      ret ← mkForallFVars #[x] ret
+      -- Eta-expand type
+      let ty ← etaLong (ret.bindingDomain!)
+      ret := ret.updateForall! ret.bindingInfo! ty ret.bindingBody!
+    return ret
+  mkLambdaFVarsEtaLong (xs : Array Expr) (b : Expr) : MetaM Expr := do
+    let mut ret := b
+    for x in xs.reverse do
+      ret ← mkLambdaFVars #[x] ret
+      -- Eta-expand type
+      let ty ← etaLong (ret.bindingDomain!)
+      ret := ret.updateLambda! ret.bindingInfo! ty ret.bindingBody!
+    return ret
+
 /-- Instantiates mvars then applies beta and eta reduction exhaustively. -/
 def betaEtaReduce (e : Expr) : MetaM Expr := do
   let e ← instantiateMVars e
