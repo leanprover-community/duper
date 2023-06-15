d1de42ae66e6ff478f08e88d15000fe5b380d7cc
Alexander Bentkamp <bentkamp@gmail.com>
2023-06-15 18:12:35 +0200

    more parsing

--- Main.lean
+++ Main.lean
@@ -1,5 +1,7 @@
 import Duper.Tactic
 import Duper.TPTP -- Note: this import is needed to make sure that TPTP is compiled for the github actions
+import Duper.TPTPParser.PrattParser
+
 
 open Lean
 open Lean.Meta
@@ -8,7 +10,9 @@ open Duper
 open ProverM
 
 #check Ne
-def run : MetaM Unit := do
+def run (input : String) : MetaM Unit := do
+
+
   let env ← getEnv
   let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Nat, levelParams := [], type := mkSort levelOne, isUnsafe := false})
   let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Bool, levelParams := [], type := mkSort levelOne, isUnsafe := false})
@@ -21,7 +25,10 @@ def run : MetaM Unit := do
   setEnv env
 
 
-  let formulas := [(← mkAppM `Ne #[mkConst `Bool.false, mkConst `Bool.false], ← mkAppM `sorryAx #[mkSort levelZero, mkConst `Bool.false],#[])]
+  let e ← TPTP.toLeanExpr input
+  
+  let formulas := [(e, ← mkAppM `sorryAx #[mkSort levelZero, mkConst `Bool.false],#[])]
+  --[(← mkAppM `Ne #[mkConst `Bool.false, mkConst `Bool.false], ← mkAppM `sorryAx #[mkSort levelZero, mkConst `Bool.false],#[])]
 
   let skSorryName ← addSkolemSorry
     let (_, state) ←
@@ -36,7 +43,7 @@ def main : List String → IO UInt32 := fun args => do
   let env ← mkEmptyEnvironment
   let _ ← Meta.MetaM.toIO
     (ctxCore := {fileName := "none", fileMap := .ofString ""}) (sCore := {env})
-    (ctx := {}) (s := {}) run
+    (ctx := {}) (s := {}) (run args[0]!)
 
   -- let _ ← Core.CoreM.toIO (ctx := {fileName := "none", fileMap := .ofString ""}) (s := {env := ← mkEmptyEnvironment}) do
 
