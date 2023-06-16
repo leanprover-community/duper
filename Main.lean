ae006485bd0c03546daec1319b24f066b6c791aa
Alexander Bentkamp <bentkamp@gmail.com>
2023-06-16 13:46:59 +0200

    PUZ137^1.p works

--- Main.lean
+++ Main.lean
@@ -9,42 +9,50 @@ open Lean.Elab.Tactic
 open Duper
 open ProverM
 
-#check Ne
-def run (input : String) : MetaM Unit := do
-
+def run (path : String) : MetaM Unit := do
+  let lines ← IO.FS.lines path
+  let lines := lines.filter fun l => ¬ l.startsWith "%"
+  let code := String.join lines.toList
 
   let env ← getEnv
-  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Nat, levelParams := [], type := mkSort levelOne, isUnsafe := false})
-  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Bool, levelParams := [], type := mkSort levelOne, isUnsafe := false})
+  let prop := mkSort levelZero
+  let type := mkSort levelOne
+  let sortu := mkSort (.param `u)
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Nat, levelParams := [], type := type, isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Iota, levelParams := [], type := type, isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Bool, levelParams := [], type := type, isUnsafe := false})
   let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Bool.false, levelParams := [], type := mkConst `Bool, isUnsafe := false})
-  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `sorryAx, levelParams := [`u], type := mkForall `α .default (mkSort (.param `u)) $ mkForall `synthetic .default (mkConst `Bool) $ mkBVar 1, isUnsafe := false})
-  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Eq, levelParams := [`u], type := mkForall `α .implicit (mkSort (.param `u)) $ ← mkArrow (mkBVar 0) $ ← mkArrow (mkBVar 1) $ mkSort levelZero, isUnsafe := false})
-  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Ne, levelParams := [`u], type := mkForall `α .implicit (mkSort (.param `u)) $ ← mkArrow (mkBVar 0) $ ← mkArrow (mkBVar 1) $ mkSort levelZero, isUnsafe := false})
-  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `True, levelParams := [], type := mkSort levelZero, isUnsafe := false})
-  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `False, levelParams := [], type := mkSort levelZero, isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `sorryAx, levelParams := [`u], type := mkForall `α .default sortu $ mkForall `synthetic .default (mkConst `Bool) $ mkBVar 1, isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Eq, levelParams := [`u], type := mkForall `α .implicit sortu $ ← mkArrow (mkBVar 0) $ ← mkArrow (mkBVar 1) $ type, isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Ne, levelParams := [`u], type := mkForall `α .implicit sortu $ ← mkArrow (mkBVar 0) $ ← mkArrow (mkBVar 1) $ type, isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `True, levelParams := [], type := prop, isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `False, levelParams := [], type := prop, isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Or, levelParams := [], type := ← mkArrow prop (← mkArrow prop prop), isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `And, levelParams := [], type := ← mkArrow prop (← mkArrow prop prop), isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Iff, levelParams := [], type := ← mkArrow prop (← mkArrow prop prop), isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Not, levelParams := [], type := ← mkArrow prop prop, isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Exists, levelParams := [`u], type := mkForall `α .implicit sortu $ ← mkArrow (← mkArrow (mkBVar 0) prop) prop, isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Duper.Skolem.some, levelParams := [`u], type := mkForall `α .implicit sortu $ ← mkArrow (← mkArrow (mkBVar 0) prop) $ ← mkArrow (mkBVar 1) (mkBVar 2), isUnsafe := false})
+  let env ← ofExceptKernelException $ env.addDecl (.axiomDecl {name := `Nonempty, levelParams := [`u], type := mkForall `α .default sortu prop, isUnsafe := false})
   setEnv env
-
-
-  let e ← TPTP.toLeanExpr input
   
-  let formulas := [(e, ← mkAppM `sorryAx #[mkSort levelZero, mkConst `Bool.false],#[])]
-  --[(← mkAppM `Ne #[mkConst `Bool.false, mkConst `Bool.false], ← mkAppM `sorryAx #[mkSort levelZero, mkConst `Bool.false],#[])]
-
-  let skSorryName ← addSkolemSorry
-    let (_, state) ←
-      ProverM.runWithExprs (ctx := {}) (s := {skolemSorryName := skSorryName})
-        ProverM.saturateNoPreprocessingClausification
-        formulas
-    match state.result with
-    | .contradiction => IO.println "SZS status Unsatisfiable"
-    | _ => IO.println "SZS status Unknown"
+  TPTP.compile code fun formulas => do
+    let skSorryName ← addSkolemSorry
+      let (_, state) ←
+        ProverM.runWithExprs (ctx := {}) (s := {skolemSorryName := skSorryName})
+          ProverM.saturateNoPreprocessingClausification
+          (Array.toList formulas)
+      match state.result with
+      | .contradiction => IO.println "SZS status Unsatisfiable"
+      | _ => IO.println "SZS status Unknown"
 
 def main : List String → IO UInt32 := fun args => do
-  let env ← mkEmptyEnvironment
-  let _ ← Meta.MetaM.toIO
-    (ctxCore := {fileName := "none", fileMap := .ofString ""}) (sCore := {env})
-    (ctx := {}) (s := {}) (run args[0]!)
-
-  -- let _ ← Core.CoreM.toIO (ctx := {fileName := "none", fileMap := .ofString ""}) (s := {env := ← mkEmptyEnvironment}) do
-
-  return 0
+  if args.length == 0 then 
+    println! "Please provide problem file."
+    return 1
+  else
+    let env ← mkEmptyEnvironment
+    let _ ← Meta.MetaM.toIO
+      (ctxCore := {fileName := "none", fileMap := .ofString ""}) (sCore := {env})
+      (ctx := {}) (s := {}) (run args[0]!)
+    return 0
