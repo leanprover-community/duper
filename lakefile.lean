2a164b945a88ff53baa650bfc83c5cc7316227f0
IndPrinciple <syxx2013050321@163.com>
2023-11-04 00:44:04 +0800

    chore

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 
 require std from git "https://github.com/leanprover/std4.git"@"main"
 
-require auto from git "https://github.com/avigad/lean-auto"
+require auto from git "https://github.com/avigad/lean-auto.git"
 
 package Duper {
   precompileModules := true
