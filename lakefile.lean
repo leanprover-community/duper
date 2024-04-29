d81253b40a2dbca7d0aac8a5dd4b8e6493148db4
JOSHCLUNE <josh.seth.clune@gmail.com>
2024-04-29 18:53:13 -0400

    Updating lean-auto

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 
 open Lake DSL
 
-require auto from git "https://github.com/leanprover-community/lean-auto.git"@"df285d9950123b2799ea71d9772d1a806f7d1e7a"
+require auto from git "https://github.com/leanprover-community/lean-auto.git"@"29d65a800b84cf2abe49c918acdfef7114b911ba"
 
 package Duper {
   precompileModules := true
