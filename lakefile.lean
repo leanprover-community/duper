34330cf3110706fc50d5e48e8f3b0ecbea0035a3
JOSHCLUNE <josh.seth.clune@gmail.com>
2024-07-20 01:27:12 -0400

    Update to lean v4.9.1

--- lakefile.lean
+++ lakefile.lean
@@ -2,8 +2,8 @@ import Lake
 
 open Lake DSL
 
-require auto from git "https://github.com/leanprover-community/lean-auto.git"@"542821345b1e8eb8e244dacafa96d677d0a55340"
-require batteries from git "https://github.com/leanprover-community/batteries" @ "v4.9.0"
+require auto from git "https://github.com/leanprover-community/lean-auto.git"@"bfb20f93b4e67029b977f218c67d8ca87ca09c9a"
+require batteries from git "https://github.com/leanprover-community/batteries" @ "v4.9.1"
 
 package Duper {
   precompileModules := true
